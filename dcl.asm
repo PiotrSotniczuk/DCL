56103c03d0138c9c591651218a288e696bcf8162
sotnii <sotnii@gmail.com>
2020-03-24 22:45:39 +0100

    sprawdza czy znak sie nie powtarza

--- dcl.asm
+++ dcl.asm
@@ -8,17 +8,24 @@ global _start
 
 section .rodata
 
-; znak nowej linii
-new_line db `\n`
-hello    db "Hello", 10
-ill_char db "bad_char", 10
-ILL_C_L  equ $ - ill_char
+bad_char db "bad_char", 10
+BAD_C_L  equ $ - bad_char
 to_less_arg db "To less arg", 10
 TO_L_ARG_L  equ $ - to_less_arg
 debug db "debug", 10
 DEBUG_L equ $ - debug
-;chars    db "12k3456789:m<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-;CHARS_L  equ $ - chars
+
+section .bss
+
+present resb 42
+
+%macro set_zeros 0     
+  mov     r9, 42
+zero_loop:                ; filling with zeros
+  mov     byte [present + r9 - 1], 0
+  dec     r9
+  jnz     zero_loop
+%endmacro
 
 section .text
 
@@ -32,33 +39,41 @@ arg_loop:
   jz      exit_less_arg    ; Napotkano zerowy wskaźnik, za malo argumentów.
   mov     ecx, MAX_LINE   ; Ogranicz przeszukiwanie do MAX_LINE znaków.
   mov     rdi, rsi        ; Ustaw adres, od którego rozpocząć szukanie.
+  set_zeros
 char_loop:
   mov     r12b, [rdi]      ; zapisz znak
-  test    r12, r12       
+  test    r12b, r12b       
   jz      check_count     ; koniec slowa
   dec     ecx
-  jz      exit_debug   ; za dlugi napis nie sprawdzam dalej
-  cmp     r12, 49       ; '1' = 49 ASCII
-  jb      exit_debug
-  cmp     r12, 90       ; '90' = 'Z' 
-  ja      exit_debug
+  jz      exit_bad_char   ; za dlugi napis nie sprawdzam dalej
+  cmp     r12b, 49       ; '1' = 49 ASCII
+  jb      exit_bad_char
+  cmp     r12b, 90       ; '90' = 'Z' 
+  ja      exit_bad_char
+  sub     r12, 49
+  mov     r8b, [present + r12]  ; patrze czy juz zajete miejsce
+  test    r8b, r8b         
+  jnz     exit_bad_char      ; jesli juz sie pojawil taki znak 
+  mov     byte [present + r12], 1 ; zajmij
   inc     rdi          ; przesuwam wskaznik
   jmp     char_loop
 check_count:
   sub     rdi, rsi        ; liczba bajtów w arg
   cmp     rdi, 42         ; 42 znaki w permutacji
-  jne     exit_ill_char   ; za duze/male argumenty
+  jne     exit_bad_char   ; za duze/male argumenty
   dec     ebx             ; i--
   jnz     arg_loop
 exit:
   mov     eax, SYS_EXIT
   xor     edi, edi        ; kod powrotu 0
   syscall
-exit_ill_char:
+
+
+exit_bad_char:
   mov     rax, SYS_WRITE
   mov     edi, STDOUT
-  mov     rsi, ill_char   ; Wypisz komunikat.
-  mov     edx, ILL_C_L          
+  mov     rsi, bad_char   ; Wypisz komunikat.
+  mov     edx, BAD_C_L          
   syscall
   mov     eax, SYS_EXIT   
   mov     rdi, 1
