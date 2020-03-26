803ad39f58d258b997d5041749a77993e8056a2e
sotnii <sotnii@gmail.com>
2020-03-26 12:39:49 +0100

    ustawienie r14 i r15 na akt bebnow

--- dcl.asm
+++ dcl.asm
@@ -2,6 +2,8 @@ SYS_WRITE equ 1
 SYS_EXIT  equ 60
 STDOUT    equ 1
 MAX_LINE  equ 50
+BUFF_SIZE equ 10
+TAB_SIZE  equ 42
 
 ; Wykonanie programu zaczyna się od etykiety _start.
 global _start
@@ -17,13 +19,14 @@ DEBUG_L equ $ - debug
 
 section .bss
 
-present resb 42
-L_1     resb 42     ; miejsce na zapis w tablicy znakow
-P_1     resb 42
-LPT     resb 24      ; miejsce na zapisanie adresu do tablicy   
+present resb TAB_SIZE
+L_1:     resb TAB_SIZE     ; miejsce na zapis w tablicy znakow
+P_1:     resb TAB_SIZE
+LPT:     resb 24      ; miejsce na zapisanie adresu do tablicy   
+buffer:  resb BUFF_SIZE
 
 %macro set_zeros 1     
-  mov     r9, 42
+  mov     r9, TAB_SIZE
 zero_loop:                ; filling with zeros
   mov     byte [%1 + r9 - 1], 0
   dec     r9
@@ -42,6 +45,18 @@ zero_loop:                ; filling with zeros
   jb      exit_bad_char
   cmp     %1, 90       ; '90' = 'Z' 
   ja      exit_bad_char
+%endmacro
+
+%macro find_akt 2
+  check_char %1
+  sub     %1, 49
+  cld
+  mov     al, %1
+  mov     ecx, TAB_SIZE
+  mov     rdi, %2
+  repne \
+  scasb
+  dec     rdi
 %endmacro
 
 section .text
@@ -73,7 +88,7 @@ _start:
         jmp     char_loop
     check_count:
       sub     rdi, rsi        ; liczba bajtów w arg
-      cmp     rdi, 42         ; 42 znaki w permutacji
+      cmp     rdi, TAB_SIZE         ; 42 znaki w permutacji
       jne     exit_bad_char   ; za duze/male argumenty
       inc     ebx             ; i++
       cmp     ebx, 3
@@ -89,10 +104,10 @@ check_perm_T:
   cmp     r9, r8          ; czy miejsce pierwotne r8 to akt sprawdzane miejsce
   jne     exit_bad_char      ; nie ma cyklu dwuelem  
   inc     r9
-  cmp     r9, 42
+  cmp     r9, TAB_SIZE
   jne     check_perm_T 
 
-;cmp rsi, [LPT + 16]
+;cmp rsi, [LPT + 16]assertion
  ; je exit_debug  
 
   mov     r9, [LPT]        ; r9 = poczatek stringu permutacji
@@ -103,12 +118,15 @@ check_perm_T:
   mov     r11, P_1
   call    near set_1
 
-
   next_arg
-  mov     r10b, [rsi]     ; akt wartosci bebnow
-  check_char r10b
-  mov     r11b, [rsi + 1]
-  check_char r11b
+  mov     r14b, [rsi]     ; akt wartosci bebnow
+  find_akt r14b, [LPT]
+  mov     r14, rdi        ; adres akt wartoosci bebna L
+
+  mov     r15b, [rsi + 1]
+  find_akt r15b, [LPT + 8]
+  mov     r15, rdi
+
   add     rbp, 8           ; adres argumentu 
   mov     rsi, [rbp]      ; adres kolejnego argumentu
   test    rsi, rsi
@@ -146,10 +164,10 @@ exit_debug:
   mov     edx, DEBUG_L          
   syscall
   mov     eax, SYS_EXIT  
-  mov     rdi, 1
+  mov     rdi, r12
   syscall
 
-  set_1:
+set_1:
   mov     r8b, 0
   mov     r10d, 0
 set_1_loop:
@@ -157,7 +175,7 @@ set_1_loop:
   mov     byte [r11 + r10], r8b ; odwrocenie permutacji
   inc     r9
   inc     r8b
-  cmp     r8b, 42
+  cmp     r8b, TAB_SIZE
   jne     set_1_loop
   ret
   
