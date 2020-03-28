92481f59c81abd67848a03614e482328f19f227e
sotnii <sotnii@gmail.com>
2020-03-28 18:36:51 +0100

    dziala dla example

--- dcl.asm
+++ dcl.asm
@@ -24,7 +24,7 @@ section .bss
 present resb TAB_SIZE
 L_1:     resb TAB_SIZE     ; miejsce na zapis w tablicy znakow
 P_1:     resb TAB_SIZE
-LPT:     resb 24      ; miejsce na zapisanie adresu do tablicy   
+LRT:     resb 24      ; miejsce na zapisanie adresu do tablicy   
 buffer:  resb BUFF_SIZE
 
 %macro set_zeros 1     
@@ -61,6 +61,12 @@ zero_loop:                ; filling with zeros
   dec     rdi
 %endmacro
 
+%macro q_plus 2
+  mov     r13b, %1
+  add     r13b, %2
+  cmp     r13b, 42
+%endmacro
+
 section .text
 
 _start:
@@ -71,7 +77,7 @@ _start:
     next_arg
     mov     ecx, MAX_LINE   ; Ogranicz przeszukiwanie do MAX_LINE znaków.
     mov     rdi, rsi        ; Ustaw adres, od którego rozpocząć szukanie.
-    mov     [LPT + ebx*8], rsi
+    mov     [LRT + ebx*8], rsi
     set_zeros present
       char_loop:
         mov     r12b, [rdi]      ; zapisz znak
@@ -109,24 +115,24 @@ check_perm_T:
   cmp     r9, TAB_SIZE
   jne     check_perm_T 
 
-;cmp rsi, [LPT + 16]assertion
+;cmp rsi, [LRT + 16]assertion
  ; je exit_debug  
 
-  mov     r9, [LPT]        ; r9 = poczatek stringu permutacji
+  mov     r9, [LRT]        ; r9 = poczatek stringu permutacji
   mov     r11, L_1
   call    near set_1
 
-  mov     r9, [LPT + 8]        ; r9 = poczatek stringu permutacji
+  mov     r9, [LRT + 8]        ; r9 = poczatek stringu permutacji
   mov     r11, P_1
   call    near set_1
 
   next_arg
   mov     r14b, [rsi]     ; akt wartosci bebnow
-  find_akt r14b, [LPT]
+  find_akt r14b, [LRT]
   mov     r14, rdi        ; adres akt wartoosci bebna L
 
   mov     r15b, [rsi + 1]
-  find_akt r15b, [LPT + 8]
+  find_akt r15b, [LRT + 8]
   mov     r15, rdi
 
   add     rbp, 8           ; adres argumentu 
@@ -146,25 +152,26 @@ read_loop:
   je      exit
   jl     exit_debug
 
+  xor     rcx, rcx
   mov     r8d, 0
-coding_loop:          ; increase modulo P
+coding_loop:          ; increase modulo R
   inc     r15
-  mov     r9, [LPT]         
+  mov     r9, [LRT + 8]         
   add     r9, 42
   cmp     r15, r9
-  jne     no_oveflow_P 
+  jne     no_oveflow_R 
   sub     r15, 42
-no_oveflow_P:
-  cmp     byte [r15], 'L'      ; check if increase L
+no_oveflow_R:
+  cmp     byte [r15], 27      ; check if increase L
   je      move_L
-  cmp     byte [r15], 'R'
+  cmp     byte [r15], 33
   je      move_L
-  cmp     byte [r15], 'T'
+  cmp     byte [r15], 35
   je      move_L
   jmp     no_overflow_L
 move_L:
   inc     r14           ; increase modulo L
-  mov     r9, [LPT + 8]
+  mov     r9, [LRT]
   add     r9, 42
   cmp     r14, r9
   jne     no_overflow_L
@@ -173,9 +180,84 @@ no_overflow_L:
 
   mov     r12b, [buffer + r8]
   check_char r12b
-  ;sub     r12b, 49  
-  ; wykonaj permutacje
+  sub     r12b, 49  
+  
+  mov     cl, [r15]
+  mov     bl, [r14]
 
+  xor     r13, r13
+
+  q_plus r12b, cl      ; Qr
+  jb      change_1
+  sub     r13b, 42
+change_1:
+  mov     r12b, r13b
+
+  mov     r9, [LRT + 8]     ;R
+  mov     r12b, [r9 + r13]
+
+  mov     r11b, 42
+  sub     r11b, cl
+  q_plus  r12b, r11b    ;Q-r
+  jb      change_2
+  sub     r13b, 42
+change_2:
+  mov     r12b, r13b
+
+  q_plus r12b, bl      ; Ql
+  jb      change_3
+  sub     r13b, 42
+change_3:
+  mov     r12b, r13b
+
+  mov     r9, [LRT]     ;L
+  mov     r12b, [r9 + r13]
+
+  mov     r11b, 42
+  sub     r11b, bl
+  q_plus  r12b, r11b    ;Q-l
+  jb      change_4
+  sub     r13b, 42
+change_4:
+  mov     r12b, r13b
+
+  mov     r9, [LRT + 16]     ;T
+  mov     r12b, [r9 + r13]
+
+  q_plus r12b, bl      ; Ql
+  jb      change_5
+  sub     r13b, 42
+change_5:
+  mov     r12b, r13b
+
+  mov     r12b, [L_1 + r13]    ;L-1
+
+  mov     r11b, 42
+  sub     r11b, bl
+  q_plus  r12b, r11b    ;Q-l
+  jb      change_6
+  sub     r13b, 42
+change_6:
+  mov     r12b, r13b
+
+  q_plus r12b, cl      ; Qr
+  jb      change_7
+  sub     r13b, 42
+change_7:
+  mov     r12b, r13b
+
+  mov     r12b, [P_1 + r13]
+
+  mov     r11b, 42
+  sub     r11b, cl
+  q_plus  r12b, r11b    ;Q-r
+  jb      change_8
+  sub     r13b, 42
+change_8:
+  mov     r12b, r13b
+  
+
+  add     r12b, 49
   mov     byte [buffer + r8], r12b
   inc     r8d
   cmp     r8d, eax
