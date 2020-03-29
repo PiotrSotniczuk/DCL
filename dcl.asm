d6b097013dec93d68590758c175b7f8925459417
sotnii <sotnii@gmail.com>
2020-03-29 10:55:10 +0200

    wszystko do read_loop

--- dcl.asm
+++ dcl.asm
@@ -6,6 +6,8 @@ STDOUT    equ 1
 MAX_LINE  equ 45
 BUFF_SIZE equ 10000
 TAB_SIZE  equ 42
+ASCII_1   equ 49
+ASCII_Z   equ 90
 
 global _start
 
@@ -13,7 +15,7 @@ section .bss
 
 present  resb 3*TAB_SIZE
 L_1:     resb TAB_SIZE     ; miejsce na zapis w tablicy znakow
-P_1:     resb TAB_SIZE
+R_1:     resb TAB_SIZE
 LRT:     resb 24      ; miejsce na zapisanie adresu do tablicy   
 buffer:  resb BUFF_SIZE
 
@@ -25,23 +27,30 @@ buffer:  resb BUFF_SIZE
 %endmacro
   
 %macro check_char 1
-  cmp     %1, 49       ; '1' = 49 ASCII
+  cmp     %1, ASCII_1       ; '1' = 49 ASCII
   jb      ex_1
-  cmp     %1, 90       ; '90' = 'Z' 
+  cmp     %1, ASCII_Z       ; '90' = 'Z' 
   ja      ex_1
 %endmacro
 
-%macro set_1 1
-  mov     r11, %1_1
-  mov     r8b, 0
-  mov     r10d, 0
+%macro set_1 2
+  mov     r9, %2        ; r9 to poczatek permutacji
+  xor     r8d, r8d
+  xor     r10d, r10d
 set_1_loop_%1:
   mov     r10b, [r9]      ; r10 = znak z permutacji
-  mov     byte [r11 + r10], r8b ; odwrocenie permutacji
-  inc     r9
-  inc     r8b
-  cmp     r8b, TAB_SIZE
+  mov     byte [%1_1 + r10], r8b ; odwrocenie permutacji
+  inc     r9            ; zwieksz wskaznik
+  inc     r8d
+  cmp     r8d, TAB_SIZE
   jne     set_1_loop_%1
+%endmacro
+
+%macro set_akt 2
+  xor     %1d, %1d
+  mov     %1b, [rsi + %2]     ; akt wartosci bebnow
+  check_char %1b
+  sub     %1b, ASCII_1
 %endmacro
 
 %macro q_plus 3
@@ -59,12 +68,12 @@ section .text
 _start:
   lea     rbp, [rsp + 8]  ; adres args[0]
   xor     ebx, ebx          ; licznik na 0
-  mov     r9, present
+  mov     r9, present       
   arg_loop:
-    next_arg
+    next_arg                ; zwieksza rbp i ustawia rsi na kolejny arg
     mov     ecx, MAX_LINE   ; Ogranicz przeszukiwanie do MAX_LINE znaków.
     mov     rdi, rsi        ; Ustaw adres, od którego rozpocząć szukanie.
-    mov     [LRT + ebx*8], rsi
+    mov     [LRT + ebx*8], rsi    
     char_loop:
         mov     al, [rdi]      ; zapisz znak
         test    al, al       
@@ -72,8 +81,8 @@ _start:
         dec     ecx
         jz      ex_1   ; za dlugi napis nie sprawdzam dalej
         check_char al
-        sub     al, 49
-        mov     byte [rdi], al  ; odejmij 49 
+        sub     al, ASCII_1
+        mov     byte [rdi], al  ; zapisz spowrotem 
         mov     r8b, [r9 + rax]  ; patrze czy juz zajete miejsce
         test    r8b, r8b         
         jnz     ex_1      ; jesli juz sie pojawil taki znak 
@@ -84,44 +93,35 @@ _start:
       sub     rdi, rsi        ; liczba bajtów w arg
       cmp     rdi, TAB_SIZE         ; 42 znaki w permutacji
       jne     ex_1   ; za duze/male argumenty
-      add     r9, 42
+      add     r9, 42        ; przesuwam present
       inc     ebx             ; i++
       cmp     ebx, 3
       jne     arg_loop
 
-
-  mov     r9, 0
+  xor     r8, r8
+  xor     ecx, ecx
 check_perm_T:
-  mov     r12b, [rsi + r9]    ; r12 = znak
-  mov     r8b, [rsi + r12] ; co jest na miejscu pierwotnym znaku w r12
-  cmp     r12b, r8b
+  mov     al, [rsi + rcx]    ; al = znak
+  mov     r8b, [rsi + rax] ; co jest na miejscu pierwotnym znaku w al
+  cmp     al, r8b
   je      ex_1           ; cykl jednoelementowy
-  cmp     r9, r8          ; czy miejsce pierwotne r8 to akt sprawdzane miejsce
+  cmp     ecx, r8d          ; czy miejsce pierwotne r8 to akt sprawdzane miejsce
   jne     ex_1      ; nie ma cyklu dwuelem  
-  inc     r9
-  cmp     r9, TAB_SIZE
+  inc     ecx
+  cmp     ecx, TAB_SIZE
   jne     check_perm_T 
-
-  mov     r9, [LRT]        ; r9 = poczatek stringu permutacji
-  set_1 L 
-
-  mov     r9, [LRT + 8]        ; r9 = poczatek stringu permutacji
-  set_1 P
+  
+  set_1 L, [LRT]                  ; ustaw perm odwrotna
+  set_1 R, [LRT + 8]
 
   next_arg
-  xor     r14, r14
-  mov     r14b, [rsi]     ; akt wartosci bebnow
-  check_char r14b
-  sub     r14b, 49
-  
-  xor     r15, r15
-  mov     r15b, [rsi + 1]
-  check_char r15b
-  sub     r15b, 49
 
-  xor     r9, r9
-  mov     r9b, [rsi + 2]
-  test    r9, r9
+  set_akt r14, 0
+  set_akt r15, 1
+
+  xor     r9d, r9d
+  mov     r9b, [rsi + 2]    ; wychodzi jesli wiecej niz 2 znaki w kluczu
+  test    r9d, r9d
   jnz     ex_1
 
   add     rbp, 8           ; adres argumentu 
@@ -163,7 +163,7 @@ no_move_L:
 
   mov     r12b, [buffer + r8]
   check_char r12b
-  sub     r12b, 49  
+  sub     r12b, ASCII_1  
   
   xor     r13, r13
 
@@ -198,14 +198,14 @@ no_move_L:
 
   q_plus r12b, r15b, Qr2      ; Qr
 
-  mov     r12b, [P_1 + r13]
+  mov     r12b, [R_1 + r13]
 
   mov     r11b, 42
   sub     r11b, r15b
   q_plus  r12b, r11b, Q_r2    ;Q-r
   
 
-  add     r12b, 49
+  add     r12b, ASCII_1
   mov     byte [buffer + r8], r12b
   inc     r8d
   cmp     r8d, eax
